d35ddcb5c9d4bebe17213dc1fa3e400e7dfafd7e
Oleh Teslenko <Teslenkooleg2017@gmail.com>
2026-08-19 11:39:02 +0300

    feat(notifications): add Notification entity and migration

---
+++ src/resources/notifications/entities/notification.entity.ts
@@ -0,0 +1,22 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('notifications')
+export class Notification {
+    @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+        id: number;
+
+    @Column('int', { name: 'userId' })
+        userId: number;
+
+    @Column('varchar', { name: 'title', length: 250 })
+        title: string;
+
+    @Column('varchar', { name: 'message', length: 500 })
+        message: string;
+
+    @Column('int', { name: 'isRead', default: 0 })
+        isRead: number;
+
+    @CreateDateColumn({ name: 'createdAt' })
+        createdAt: Date;
+}
